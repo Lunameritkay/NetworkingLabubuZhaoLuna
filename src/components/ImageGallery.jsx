91ef1eb1984aada35fb254ed9b119a1441bcbbd6
Your Name <you@example.com>
2025-10-27 19:53:37 +0900

    add: image gallery

--- src/components/ImageGallery.jsx
+++ src/components/ImageGallery.jsx
@@ -1,11 +1,42 @@
+import React, { useState } from 'react';
+import { Image } from 'antd';
 import '../App.css'
 
 function ImageGallery () {
-
+  const pictures = [{
+    src: "src/assets/20250422_091913_284240____6_____1200x1200.jpg"
+  }, {
+    src: "src/assets/20250422_091913_427360____3_____1200x1200.jpg"
+  }, {
+    src: "src/assets/20250422_091913_429614____7_____1200x1200.jpg"
+  }, {
+    src: "src/assets/20250422_091913_592227____4_____1200x1200.jpg"
+  }, {
+    src: "src/assets/20250422_091913_967169____5_____1200x1200.jpg"
+  }]
+  const [index, setIndex] = useState(0)
+  const handleClick = (i) => {
+    setIndex(i)
+  }
   return (
-    <>
-    <div>ImageGallery</div>
-    </>
+    <div className='imageGalleryWrapper'>
+      <div className='shortCutWrapper'>
+        {pictures.map((item, index) => (
+          <img
+            className='shortcut'
+            preview={false}
+            src={item.src}
+            onClick={() => handleClick(index)}
+            key={index}
+          />
+        ))}
+      </div>
+        <Image
+          width={400}
+          preview={false}
+          src={pictures[index].src}
+        />
+    </div>
   )
 }
 
